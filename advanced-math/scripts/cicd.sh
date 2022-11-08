68f76adbd4d74ae49b9871770574546178a50549
IvanC <ivan.cavlek1403@gmail.com>
2022-11-08 15:27:28 +0100

    Script command work, don't know how

--- advanced-math/scripts/cicd.sh
+++ advanced-math/scripts/cicd.sh
@@ -23,11 +23,11 @@ case $1 in
         done;;
     "build")
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            cargo build-sbf --manifest-path=./src/$program/Cargo.toml --sbf-out-dir=./dist/program
         done;;
     "deploy")
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            cargo build-sbf --manifest-path=./src/$program/Cargo.toml --sbf-out-dir=./dist/program
             solana program deploy dist/program/$program.so
         done;;
     "reset-and-build")
@@ -35,13 +35,13 @@ case $1 in
         for x in $(solana program show --programs | awk 'RP==0 {print $1}'); do 
             if [[ $x != "Program" ]]; 
             then 
-                solana program close $x; 
+                solana program close $x --bypass-warning; 
             fi
         done
         rm -rf dist/program
         for program in "${SOLANA_PROGRAMS[@]}"; do
             cargo clean --manifest-path=./src/$program/Cargo.toml
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            cargo build-sbf --manifest-path=./src/$program/Cargo.toml --sbf-out-dir=./dist/program
             solana program deploy dist/program/$program.so
         done
         npm install
